2e72e5ad3818aba84be559b7c9ab6cf895a813e0
shrvya <shrivyashetty@gmail.com>
2022-12-06 16:12:56 +0530

    added:bootstrap

--- seeds/index.js
+++ seeds/index.js
@@ -23,10 +23,15 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const cost=Math.floor(Math.random() * 300)+100;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image:'https://source.unsplash.com/collection/483251',
+            price:cost,
+            description:"Loreum ipsium woods"
         })
+        console.log(camp)
         await camp.save();
     }
 }
